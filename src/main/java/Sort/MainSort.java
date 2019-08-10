3ea5cdf9be9b310753c69bbe9f09f50f07f64583
Marta Przybyło <marta.m.przybylo@gmail.com>
2019-08-10 15:45:32 +0200

    Bubble Sort

---
+++ src/main/java/Sort/MainSort.java
@@ -0,0 +1,13 @@
+package Sort;
+
+public class MainSort {
+    public static void main(String[] args) {
+        int [] table = new int[] {3,2,6,1,3,7};
+        BubbleSort bubbleSort = new BubbleSort();
+        table=bubbleSort.sort(table);
+
+        for (int n:table){
+            System.out.print(n + " ");
+        }
+    }
+}
