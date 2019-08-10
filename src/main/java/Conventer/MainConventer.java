237a56a13aff8c68a1a7fe03c738f2032cee0ec9
Marta Przybyło <marta.m.przybylo@gmail.com>
2019-08-10 11:22:05 +0200

    convnenter base something

---
+++ src/main/java/Conventer/MainConventer.java
@@ -0,0 +1,9 @@
+package Conventer;
+
+public class MainConventer {
+
+    public static void main(String[] args) {
+        NumbersConventer numbersConventer = new NumbersConventer();
+        System.out.println(numbersConventer.binaryToDecimal("00101010"));
+    }
+}
---
+++ src/main/java/Conventer/NumbersConventer.java
@@ -0,0 +1,15 @@
+package Conventer;
+
+public class NumbersConventer {
+    public String binaryToDecimal(String binaryNumber){
+        int power = 0;
+        int sum = 0;
+        char [] binaryCharArray = binaryNumber.toCharArray();
+        for (int i = binaryCharArray.length-1; i>=0; i--){
+            int curentBit = Character.getNumericValue(binaryCharArray[i]);
+            sum += curentBit* Math.pow(2, power++);
+        }
+        return Integer.toString(sum);
+
+    }
+}
